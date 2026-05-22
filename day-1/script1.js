5e0d42ded8f1b0d1e7b97b09143a9e1da8b7b8f0
Rupesh kushwaha <rupeshkushwaha03032@gmail.com>
2026-05-22 17:31:23 +0530

    day1 update

--- day-1/script1.js
+++ day-1/script1.js
@@ -1,3 +1,36 @@
 "use strict";
-let aa = 12;
-console.log(aa);
+// let a: number = 12;
+// let b: string = " 12";
+// let c: boolean = true;
+// let d: null = null;
+// let arr: Array<number> = [1, 2, 3, 4];
+// let tuple:[number , string , boolean , null] = [1 , "dgf" , true , null]
+// let typeAny:any
+// typeAny = 12
+// typeAny =" 12"
+// typeAny = null
+// typeAny = undefined
+// typeAny = true
+// let un:unknown
+// un = 12
+// un = null
+// function infiniteRun():never {
+//   let o = 0;
+//   while (true) {
+//     console.log("run infinite loop :", o);
+//     o++;
+//   }
+// }
+// infiniteRun();
+// console.log("test");
+// function typeVoidCheck():void {
+//   console.log("check void");
+// }
+// console.log(typeVoidCheck());
+var Direction;
+(function (Direction) {
+    Direction["left"] = "LEFT";
+    Direction["right"] = "RIGHT";
+})(Direction || (Direction = {}));
+console.log(Direction.left);
+console.log(Direction.right);
--- day-1/script1.ts
+++ day-1/script1.ts
@@ -1,10 +1,43 @@
-let a: number = 12;
-let b: string = " 12";
-let c: boolean = true;
-let d: null = null;
+// let a: number = 12;
+// let b: string = " 12";
+// let c: boolean = true;
+// let d: null = null;
 
-let arr: Array<number> = [1, 2, 3, 4];
+// let arr: Array<number> = [1, 2, 3, 4];
 
-let tuple:[number , string , boolean , null] = [1 , "dgf" , true , null]
+// let tuple:[number , string , boolean , null] = [1 , "dgf" , true , null]
 
-// 36:0
+// let typeAny:any
+// typeAny = 12
+// typeAny =" 12"
+// typeAny = null
+// typeAny = undefined
+// typeAny = true
+
+// let un:unknown
+// un = 12
+// un = null
+
+// function infiniteRun():never {
+//   let o = 0;
+//   while (true) {
+//     console.log("run infinite loop :", o);
+//     o++;
+//   }
+// }
+
+// infiniteRun();
+// console.log("test");
+
+// function typeVoidCheck():void {
+//   console.log("check void");
+// }
+// console.log(typeVoidCheck());
+
+enum Direction {
+  left = "LEFT",
+  right = "RIGHT",
+}
+
+console.log(Direction.left);
+console.log(Direction.right);
